c00fde2f9816d3a5da399eee1d0dedc3723c3d31
Derek <dduong03@uoguelph.ca>
2024-02-12 17:51:44 -0500

    Added circle animation and counting to preload screen

--- script.js
+++ script.js
@@ -1,40 +1,65 @@
-// Select the Projects menu item and add a click event listener
-document.querySelector('.menu-item-projects').addEventListener('click', function() {
-    var projectsMenu = document.getElementById('projects-menu');
-    
-    // Toggle the menu's visibility using GSAP
-    if (projectsMenu.classList.contains('hidden')) {
-      // Remove the 'hidden' class and animate the menu into view
-      projectsMenu.classList.remove('hidden');
-      gsap.to(projectsMenu, { duration: 0.5, xPercent: -100, ease: 'power1.out' });
-    } else {
-      // Animate the menu out of view and then add the 'hidden' class back
-      gsap.to(projectsMenu, { duration: 0.5, xPercent: 100, ease: 'power1.in', onComplete: () => projectsMenu.classList.add('hidden') });
-    }
-  });
+//import GSAP
+// Adding the load screen animation
+function startLoader() {
+  let counterElement = document.querySelector(".counter");
+  let currentValue = 0;
 
-  document.addEventListener('DOMContentLoaded', (event) => {
-    const projectsMenu = document.getElementById('projects-menu');
-    if (projectsMenu) {
-      projectsMenu.classList.add('hidden');
+  function updateCounter() {
+    if (currentValue === 100) {
+      return;
     }
-  });
-  
-  
-//   Adds event listener to the menu so that an underline can appear \
-// Under the item
 
-document.querySelector('menu-container').addEventListnet('click', function(e) {
-    if (e.target.classList.contains('menu-item')) {
-        document.querySelector('.menu-item.active').classList.remove('active');
-        e.target.classList.add('active');
+    currentValue += Math.floor(Math.random() * 10) + 1;
+    if (currentValue > 100) {
+      currentValue = 100;
     }
+
+    counterElement.textContent = currentValue + "%";
+    let delay = Math.floor(Math.random() * 200) + 250;
+    setTimeout(updateCounter, delay);
+  }
+  updateCounter();
+}
+// Call the startLoader function when the DOM is ready
+startLoader();
+
+gsap.from(".circles", 2, {
+  top: "100%",
+  ease: "elastic.out",
+  delay: 0.5,
 });
 
-gsap.to("#whirlpool", {
-    rotation: 360,
-    duration: 5,
-    repeat: -1, // This makes the animation loop infinitely
-    ease: "linear",
-    transformOrigin: "50% 50%" // Ensures the whirlpool spins around its center
+gsap.to(".circle-inner", 1, {
+  width: "75px",
+  height: "75px",
+  ease: "power4.inOut",
+  delay: 2,
 });
+
+
+gsap.to(".circle-inner-rotator", 1, {
+  scale: 1,
+  ease: "power4.inOut",
+  delay: 2.5,
+});
+
+gsap.to(".circles", 1.5, {
+  rotation: 360,
+  ease: "power4.inOut",
+  delay: 3,
+})
+
+gsap.to(".block", 1.5, {
+  rotation: 360,
+  ease: "power4.inOut",
+  delay: 3,
+})
+
+gsap.to(".block", 1.5, {
+  rotation: 360,
+  ease: "power4.inOut",
+  delay: 3,
+})
+
+// // Print test
+// console.log("Hello World");
