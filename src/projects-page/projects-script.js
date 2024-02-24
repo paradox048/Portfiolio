39ec9ef988a91fb53215e3f9239964519c22ffa0
Derek <dduong03@uoguelph.ca>
2024-02-23 23:11:34 -0500

    finished button styling - need to make it responsive

--- src/projects-page/projects-script.js
+++ src/projects-page/projects-script.js
@@ -48,3 +48,35 @@ window.addEventListener("pageshow", function () {
   // Correctly toggle the icon
   updateIcon(savedTheme);
 });
+// control the project-experience btns
+// Button control for the project and experience toggle
+const projectBtn = document.getElementById("project-page-projects-btn");
+const experienceBtn = document.getElementById("project-page-experience-btn");
+
+// on the load in make sure that the project button is active
+projectBtn.classList.add("active-project-page-btn");
+experienceBtn.classList.add("inactive-project-page-btn");
+
+// on click of the project button make sure that the project button is active and the experience button is inactive
+// then show the project section and hide the experience section
+projectBtn.addEventListener("click", function () {
+  if (projectBtn.classList.contains("inactive-project-page-btn")) {
+    projectBtn.classList.remove("inactive-project-page-btn");
+  }
+  projectBtn.classList.add("active-project-page-btn");
+  experienceBtn.classList.remove("active-project-page-btn");
+  experienceBtn.classList.add("inactive-project-page-btn");
+
+  // load in the projects section if not already loaded
+  // check if the display is none and if it is then load in the projects section
+});
+
+experienceBtn.addEventListener("click", function () {
+  if (experienceBtn.classList.contains("inactive-project-page-btn")) {
+    experienceBtn.classList.remove("inactive-project-page-btn");
+  }
+  experienceBtn.classList.add("active-project-page-btn");
+
+  projectBtn.classList.remove("active-project-page-btn");
+  projectBtn.classList.add("inactive-project-page-btn");
+});
